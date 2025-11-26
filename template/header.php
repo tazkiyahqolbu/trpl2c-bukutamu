d3c68dc8316803b779d0580865372bc21a86664a
tazkiyahqolbu <tazkiyahqolbu2@gmail.com>
2025-11-26 08:58:16 +0700

    bikin tamplate

---
+++ template/header.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html lang="id">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Dokumen Dasar</title>
+  <style>
+    body{
+      margin:0;
+      font-family:Arial, Helvetica, sans-serif;
+      line-height:1.5;
+    }
+  </style>
+</head>
+<body>
+  <h1>Halo Dunia</h1>
+  <p>Ini template HTML dasar.</p>
+</body>
+</html>

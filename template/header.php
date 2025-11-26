1398414a881357a13724b7abd86d782333cb00e4
tazkiyahqolbu <tazkiyahqolbu2@gmail.com>
2025-11-26 09:02:14 +0700

    ganti tittle

--- template/header.php
+++ template/header.php
@@ -3,17 +3,8 @@
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>Dokumen Dasar</title>
-  <style>
-    body{
-      margin:0;
-      font-family:Arial, Helvetica, sans-serif;
-      line-height:1.5;
-    }
-  </style>
+  <title>Buku Tamu</title>
 </head>
 <body>
-  <h1>Halo Dunia</h1>
-  <p>Ini template HTML dasar.</p>
 </body>
 </html>
